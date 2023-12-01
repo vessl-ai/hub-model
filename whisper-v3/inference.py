501e19f416a8eb30b5e6807098b004ed5a56268f
Sanghyuk Lee <sanghyuk@vessl.ai>
2023-12-01 13:04:09 +0900

    Fix paths

--- whisper-v3/inference.py
+++ whisper-v3/inference.py
@@ -1,13 +1,13 @@
 from datasets import load_from_disk
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 
-model_path = "/root/ckpt/Whisper-large-v3"
+model_path = "/model/Whisper-large-v3"
 
 processor = WhisperProcessor.from_pretrained(model_path)
 model = WhisperForConditionalGeneration.from_pretrained(model_path)
 model.config.forced_decoder_ids = None
 
-ds = load_from_disk("/root/data/librispeech_asr_clean/test")
+ds = load_from_disk("/dataset/librispeech_asr_clean_test")
 
 print("===========")
 for i in range(5):
