572ac149e72f24b5ffa16349002627008acd46ca
Sanghyuk Lee <shylee2021@gmail.com>
2023-12-04 15:19:41 +0900

    VSSL-6828 Add SSD-1B inference and streamlit codes (#1)
    * Add SSD inference and streamlit codes
    
    * Fix checkpoint path
    
    * Update requirements.txt
    
    * Modify README.md

---
+++ SSD-1B/README.md
@@ -0,0 +1,23 @@
+# SSD-1B
+
+SSD-1B: Faster, smaller SDXL variant excelling in high-quality text-to-image generation with diverse dataset training, incorporating Grit and Midjourney data.
+
+# Description
+
+The Segmind Stable Diffusion Model (`SSD-1B`) is a distilled **50% smaller version** of the Stable Diffusion XL (SDXL), offering a **60% speedup** while maintaining high-quality text-to-image generation capabilities. It has been trained on diverse datasets, including Grit and Midjourney scrape data, to enhance its ability to create a wide range of visual content based on textual prompts.
+
+# Quickstart
+
+Recreate the model's outcome using a concise code snippet (specifics of the YAML code can be found below).
+
+```sh
+# clone repository and move to directory
+git clone https://github.com/vessl-ai/hub-model
+cd SSD-1B
+
+# run by cli
+python ssd_1b_inference.py
+
+# run streamlit on local
+streamlit run ssd_1b_streamlit.py --server.port=80
+```
---
+++ SSD-1B/ssd_1b_streamlit.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+
+import streamlit as st
+import torch
+from diffusers import StableDiffusionXLPipeline
+from PIL import Image
+
+# To run streamlit, setup streamlit port as 80 and run 'streamlit run SSD-1B_streamlit.py --server.port=80'
+st.set_page_config(layout="wide")
+
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "/model_/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
+)
+pipe.to("cuda")
+
+Input_query = "An astronaut riding a green horse"  # Your prompt here
+prompt = st.text_input("Input Query", Input_query)
+negative = "ugly, blurry, poor quality, scary"  # Negative prompt here
+neg_prompt = st.text_input("Negative Things", negative)
+image = pipe(prompt=prompt, negative_prompt=neg_prompt).images[0]
+
+process = f"python SSD-1B_inference.py --prompt '{prompt} --neg_prompt {neg_prompt}'"
+
+generated_image = prompt + ".jpg"
+image.save(generated_image)
+image_path = "./" + generated_image
+generated_img = Image.open(image_path)
+st.image(generated_img)
