3885ff6afadbf7359064ccc1829b11f71172b7d2
Sanghyuk Lee <shylee2021@gmail.com>
2023-12-07 18:55:54 +0900

    VSSL-6825 Add finetuning llama 2 (#5)
    * Add finetuning script
    
    * Add requirements.txt
    
    * Modify configuration
    
    * Add README.md

---
+++ llama2-finetuning/README.md
@@ -0,0 +1,24 @@
+# Finetuning Llama2
+
+Finetuning Llama 2 with a small code instruction dataset in Alpaca format
+
+## Description
+
+Llama 2 is a large language model (LLM) developed by Meta. It is open-sourced with codes and various weights pre-trained and finetuned on, varying in size of parameter counts from 7 billion to 70 billion. In this repository, llama 2 7B model will be finetuned on a code instruction dataset which consists of 18k samples and is in format of Alpaca instruction. 
+
+While training, learning rate, training loss and validation loss will be logged on `Plots` page.
+
+## Quickstart
+
+Modify `config.yaml` to your own configuration and run following:
+
+```sh
+pip install -r requirements.txt
+python finetuning.py
+```
+
+## Reference
+
+ - Arxiv: <https://arxiv.org/abs/2307.09288>
+ - Blog: <https://ai.meta.com/resources/models-and-libraries/llama/>
+ - Repo: <https://github.com/facebookresearch/llama>
---
+++ llama2-finetuning/finetuning.py
@@ -0,0 +1,120 @@
+import torch
+import vessl
+from datasets import load_from_disk
+from omegaconf import OmegaConf
+from peft import get_peft_config, get_peft_model, prepare_model_for_int8_training
+from transformers import (
+    DataCollatorForSeq2Seq,
+    LlamaForCausalLM,
+    LlamaTokenizer,
+    Trainer,
+    TrainerCallback,
+    TrainingArguments,
+)
+
+config = OmegaConf.load("./config.yaml")
+
+model = LlamaForCausalLM.from_pretrained(
+    config.model_name,
+    load_in_8bit=True if config.quantization else None,
+    device_map="auto" if config.quantization else None,
+)
+
+tokenizer = LlamaTokenizer.from_pretrained(config.model_name)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+
+if config.quantization:
+    model = prepare_model_for_int8_training(model)
+
+if config.peft.enable:
+    peft_config = get_peft_config(OmegaConf.to_container(config.peft.config))
+    model = get_peft_model(model, peft_config)
+
+
+# dataset
+def tokenize(sample, add_eos_token=True):
+    prompt = sample["prompt"]
+
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=256,
+        padding=False,
+        return_tensors=None,
+    )
+    if (
+        result["input_ids"][-1] != tokenizer.eos_token_id
+        and len(result["input_ids"]) < 256
+        and add_eos_token
+    ):
+        result["input_ids"].append(tokenizer.eos_token_id)
+        result["attention_mask"].append(1)
+
+    result["labels"] = result["input_ids"].copy()
+
+    return result
+
+
+ds = load_from_disk(config.dataset_path)
+processed_ds = ds.map(tokenize)
+
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+)
+
+train_config = config.training
+training_arguments = TrainingArguments(
+    per_device_train_batch_size=train_config.batch_size,
+    gradient_accumulation_steps=train_config.grad_acc_steps,
+    warmup_steps=train_config.warmup_steps,
+    num_train_epochs=train_config.num_train_epochs,
+    # max_steps=train_config.max_steps,
+    learning_rate=train_config.learning_rate,
+    fp16=train_config.use_fp16,
+    logging_steps=train_config.logging_steps,
+    evaluation_strategy="steps",
+    save_strategy="steps",
+    eval_steps=train_config.eval_steps,
+    save_steps=train_config.save_steps,
+    output_dir=train_config.intermediate_dir,
+    save_total_limit=train_config.save_limit,
+    load_best_model_at_end=True,
+)
+
+
+class VesslLogCallback(TrainerCallback):
+    def on_log(self, args, state, control, logs=None, **kwargs):
+        if "eval_loss" in logs.keys():
+            payload = {
+                "eval_loss": logs["eval_loss"],
+            }
+            vessl.log(step=state.global_step, payload=payload)
+        elif "loss" in logs.keys():
+            payload = {
+                "train_loss": logs["loss"],
+                "learning_rate": logs["learning_rate"],
+            }
+            vessl.log(step=state.global_step, payload=payload)
+
+
+trainer = Trainer(
+    model=model,
+    train_dataset=processed_ds["train"],
+    eval_dataset=processed_ds["validation"],
+    args=training_arguments,
+    data_collator=data_collator,
+    callbacks=[VesslLogCallback],
+)
+model.config.use_cache = False
+
+# old_state_dict = model.state_dict
+# model.state_dict = (
+#     lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+# ).__get__(model, type(model))
+
+model = torch.compile(model)
+trainer.train()
+
+model.save_pretrained(train_config.output_dir)
